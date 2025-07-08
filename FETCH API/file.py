324bb7415a9180d819539164f1aa8d4bf779a80e
Sabarehmani <sabarehmani287@gmail.com>
2025-07-08 02:27:18 -0500

    work

---
+++ FETCH API/file.py
@@ -0,0 +1,14 @@
+import requests
+respone = requests.get("https://jsonplaceholder.typicode.com/todos/")
+print(respone)
+print(respone.status_code)
+data = respone.json()
+print(data)
+print(data[2]["title"])
+print(data[25]["id"])
+
+
+
+
+
+
