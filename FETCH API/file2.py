9dbbb908fb8a1c7f0a198e599115fa3ff7d21ca4
Sabarehmani <sabarehmani287@gmail.com>
2025-07-12 05:13:42 -0500

    more work

--- FETCH API/file2.py
+++ FETCH API/file2.py
@@ -1,9 +1,10 @@
-import requests
-response = requests.get("https://randomuser.me/api/")
-data = response.json()
-print(data)
+# import requests
+# response = requests.get("https://randomuser.me/api/")
+# data = response.json()
+# print(data)
 
-print(data["results"][0]["gender"])
-print(data["results"][0]["name"]["first"])
-print(data["results"][0]["name"]["last"])
+# print(data["results"][0]["gender"])
+# print(data["results"][0]["name"]["first"])
+# print(data["results"][0]["name"]["last"])
+
 
