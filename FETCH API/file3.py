17b7862855221dce20f5df4a82240ba6aa8a588c
Sabarehmani <sabarehmani287@gmail.com>
2025-07-16 03:28:09 -0500

    more work

---
+++ FETCH API/file3.py
@@ -0,0 +1,27 @@
+import numpy as np
+# arr= np.array((5,5))
+# print(arr)
+# print(arr.shape) 
+# print(arr.size)
+# print(arr.ndim)
+# print(arr.dtype)
+
+
+
+li = [1,2,3,4,5] 
+li2= []
+for i in li:
+    li2.append(i*2)
+print(li2)
+
+
+
+
+
+
+
+
+
+
+
+
