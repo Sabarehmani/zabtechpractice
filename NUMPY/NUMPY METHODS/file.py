a14609b6a02a666c795e2a4c273595aa847d0e5e
Sabarehmani <sabarehmani287@gmail.com>
2025-07-26 02:45:04 -0500

    more work

---
+++ NUMPY/NUMPY METHODS/file.py
@@ -0,0 +1,27 @@
+import numpy as np
+myarr = np.array([3,5,67,89,54,23] , np.int8)
+print(myarr.shape)
+print(myarr.dtype)
+print(myarr[0,1])
+
+print(myarr)
+
+
+#ARRAY CREATIONS METHODS:
+#CONVERSION FROM OTHER PYTHON STRUCTURES:
+
+listarray= np.array ([1,2,3,4,5] , [3,4,5,6,7], [7,8,9,6,5,])
+print(listarray)
+print(listarray.dtype)
+print(listarray.size)
+
+
+np.array({34,56,78,98})   #dictionary
+print(np.array)
+
+#INTRINSIC NUMPY ARRAY CREATION OBJECTS:  (ARRANGE,ONES,ZEROS ETC)
+
+
+
+
+
---
+++ PANDAS/pandas/file.py
@@ -0,0 +1,18 @@
+import numpy as np 
+arr = np.array([2,4,5,6, np.nan, 6,7])
+arr2 = np.nan_to_num(arr , nan=22)
+print(np.isnan(arr2))
+print(arr2)
+
+arr = np.array([2,3,4 np.inf,5,6, -np.inf])
+print(np.isinf(arr))
+
+arr2 = np.nan_to_num(arr, posinf=20, neginf=40)
+print(np.isinf(arr2))
+print(arr2)
+
+import pandas as pd
+data = {
+    "name" : ["usman" , "adnan" , "saba" , "kamran"]
+    "age" : [27,25,23,20]
+}
